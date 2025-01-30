fca6c81a0b60c6b4a5c085d4e9b00e906aa686a1
Tomas Navarro <tomas.navarro.m@outlook.com>
2025-01-29 22:56:29 -0300

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,21 +9,21 @@ const Navbar = () => {
                 <div className="d-flex justify-content-between align-items-center">
                     <a className="navbar-brand" href="#">Pizzería Mamma Mia!</a>
                     <div className="navbar-nav gap-3 d-flex flex-row">
-                        <a className="nav-link active" aria-current="page" href="#">🍕Home</a>
-                        <a className="nav-link active" aria-current="page" href="#">🔐Profile</a>
+                        <a className="nav-link active border border-light px-2 rounded" aria-current="page" href="#">🍕Home</a>
+                        <a className="nav-link active border border-light px-2 rounded" aria-current="page" href="#">🔐Profile</a>
                         {token ? (
                             <>
-                                <a className="nav-link active" aria-current="page" href="#">🔐 Profile</a>
-                                <a className="nav-link active" aria-current="page" href="#">🔐 Logout</a>
+                                <a className="nav-link active border border-light px-2 rounded" aria-current="page" href="#">🔐 Profile</a>
+                                <a className="nav-link active border border-light px-2 rounded" aria-current="page" href="#">🔐 Logout</a>
                             </>
                         ) : (
                             <>
-                                <a className="nav-link active" aria-current="page" href="#">🔓 Login</a>
-                                <a className="nav-link active" aria-current="page" href="#">🔓 Register</a>
+                                <a className="nav-link active border border-light px-2 rounded" aria-current="page" href="#">🔓 Login</a>
+                                <a className="nav-link active border border-light px-2 rounded" aria-current="page" href="#">🔓 Register</a>
                             </>
                         )}
 
-                        <a className="nav-link active" aria-current="page" href="#">💰 Total: ${total.toLocaleString()}</a>
+                        <a className="nav-link active border border-info rounded text-info position-absolute px-2 top-0 end-0 me-2 mt-2" aria-current="page" href="#">🛒 Total: ${total.toLocaleString()}</a>
                     </div>
                 </div>
             </nav>
